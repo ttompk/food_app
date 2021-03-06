8fbc93605d615bd93698c72c866a81aa01b54936
ttompk <ttompkasu@gmail.com>
2021-03-06 14:39:19 -0800

    pyaudio playback update

--- picovoice_dev/pyaudio_play.py
+++ picovoice_dev/pyaudio_play.py
@@ -1,4 +1,8 @@
 from wave_func import AudioFile
+import os
+
+os.system('aplay -Dhw:1 -d 10 ~/food_app/picovoice_dev/piano2.wav')
+
 
 #AudioFile("play", "/Users/kenwyn/datascience/food_app/picovoice_dev/piano2.wav")
 AudioFile("play", "/home/pi/food_app/picovoice_dev/piano2.wav")
