b169011d4763ad40f899abcc94ea2e29bc06c0b6
ttompk <ttompkasu@gmail.com>
2021-03-06 15:07:15 -0800

    pyaudio playback update

--- picovoice_dev/pyaudio_play.py
+++ picovoice_dev/pyaudio_play.py
@@ -1,9 +1,37 @@
-from wave_func import AudioFile
+#from wave_func import AudioFile
 import os
 
-wav_file = 'piano2.wav'
-os.system(f'aplay -Dhw:1 -d 10 ~/food_app/picovoice_dev/{wav_file}')
+# NOTE: # run getDeviceInfo.py to get index = input device id
+
+
+def play_file(wav_file='', RESPEAKER_INDEX=1):
+    if wave_file == '':
+        wav_file = 'piano2.wav'
+  
+    # play the sound file using command line
+    os.system(f'aplay -Dhw:{RESPEAKER_INDEX} -d 10 ~/food_app/picovoice_dev/{wav_file}')
+
+def record_file(wav_file='', nseconds=5, RESPEAKER_INDEX=1):
+    if wav_file=='':
+        wav_file = 'test_record.wav'
+    # record sound
+    os.system(f'arecord -f S16_LE -d {nseconds} -r 16000 -Dhw:{RESPEAKER_INDEX} /tmp/{test_record.wav} -c 2')
+
 
 # this works but takes a long time to load
 # makes a popping sound after file ran
 #AudioFile("play", "/home/pi/food_app/picovoice_dev/piano2.wav")
+
+if __name__=='__main__':
+    wav_file = input("File name (empty = default): ")
+    print(wav_file)
+    resp = input("'play' or 'record: ")
+    if resp == 'play':
+        play_file(wav_file)
+    elif resp == 'record':
+        nseconds = input('n seconds (empty = 5): ')
+        if nseconds == '':
+            nseconds = 5
+        record_file(wav_file, nseconds)
+    else:
+        print("play or record required")
---
+++ picovoice_dev/rhino_template.py
@@ -0,0 +1,36 @@
+### 
+# rhino template file
+###
+
+# initial code here: https://github.com/Picovoice/rhino  
+
+import pvrhino
+
+# set variables for context file: '/absolute/path/to/context'
+Path_To_Context = 'home/pi/food_app/picovoice_dev/context'  # default' 
+Sample_Rate = 16000   # default 16000  
+Frame_Length = 521    # default 512  
+
+
+handle = pvrhino.create(context_path = Path_To_Context)
+
+def get_next_audio_frame():
+    pass
+
+while True:
+    is_finalized = handle.process(get_next_audio_frame())
+
+    if is_finalized:
+        inference = handle.get_inference()
+        if not inference.is_understood:
+            # add code to handle unsupported commands
+            # play wav file saying: "I'm sorry, I didn't understand the command."
+            pass
+        else:
+            intent = inference.intent
+            slots = inference.slots
+            # add code to take action based on inferred intent and slot values
+
+
+# Finally, when done be sure to explicitly release the resources using:
+handle.delete()
