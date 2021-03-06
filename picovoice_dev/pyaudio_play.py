c2b3233b1b1a8c865f434349674227927138f460
ttompk <ttompkasu@gmail.com>
2021-03-06 14:42:16 -0800

    pyaudio playback update

--- picovoice_dev/pyaudio_play.py
+++ picovoice_dev/pyaudio_play.py
@@ -1,8 +1,9 @@
 from wave_func import AudioFile
 import os
 
-os.system('aplay -Dhw:1 -d 10 ~/food_app/picovoice_dev/piano2.wav')
+wav_file = 'piano2.wav'
+os.system(f'aplay -Dhw:1 -d 10 ~/food_app/picovoice_dev/{wav_file}')
 
-
-#AudioFile("play", "/Users/kenwyn/datascience/food_app/picovoice_dev/piano2.wav")
-AudioFile("play", "/home/pi/food_app/picovoice_dev/piano2.wav")
+# this works but takes a long time to load
+# makes a popping sound after file ran
+#AudioFile("play", "/home/pi/food_app/picovoice_dev/piano2.wav")
