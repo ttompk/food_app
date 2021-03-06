8d1ad955ef3437c2b867a91baf82f771467c9ab4
ttompk <ttompkasu@gmail.com>
2021-03-06 13:48:05 -0800

    pyaudio dev file update

--- picovoice_dev/pyaudio_play.py
+++ picovoice_dev/pyaudio_play.py
@@ -1,4 +1,4 @@
 from wave_func import AudioFile
 
-AudioFile("play", "/Users/kenwyn/datascience/food_app/picovoice_dev/piano2.wav")
-
+#AudioFile("play", "/Users/kenwyn/datascience/food_app/picovoice_dev/piano2.wav")
+AudioFile("play", "/home/pi/food_app/picovoice_dev/piano2.wav")
